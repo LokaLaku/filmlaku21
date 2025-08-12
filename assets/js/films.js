75b03d1fcce8588914435387fa1364e62357f81e
LokaLaku <lokalakutrip@gmail.com>
2025-08-13 02:39:58 +0700

    Create films.js

---
+++ assets/js/films.js
@@ -0,0 +1,21 @@
+// films.js
+const films = {
+    film1: {
+        title: "Home Sweet Loan",
+        img: "assets/img/home-sweet-loan.jpg",
+        video: "https://www.youtube.com/embed/tgbNymZ7vqY",
+        rekomendasi: ["film2", "film3"]
+    },
+    film2: {
+        title: "Pulang",
+        img: "assets/img/pulang.jpg",
+        video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+        rekomendasi: ["film1", "film3"]
+    },
+    film3: {
+        title: "Freedom",
+        img: "assets/img/freedom.jpg",
+        video: "https://www.youtube.com/embed/oHg5SJYRHA0",
+        rekomendasi: ["film1", "film2"]
+    }
+};
