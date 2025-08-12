5b6dc3dd3b9042f76fe4e97b7d688f3a68e343b2
LokaLaku <lokalakutrip@gmail.com>
2025-08-13 02:52:30 +0700

    Add files via upload

--- assets/js/films.js
+++ assets/js/films.js
@@ -1,21 +1,23 @@
-// assets/js/films.js
 const films = {
     film1: {
         title: "Home Sweet Loan",
         img: "assets/img/home-sweet-loan.jpg",
         video: "https://www.youtube.com/embed/tgbNymZ7vqY",
+        rating: "8.2",
         rekomendasi: ["film2", "film3"]
     },
     film2: {
         title: "Pulang",
         img: "assets/img/pulang.jpg",
         video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+        rating: "7.9",
         rekomendasi: ["film1", "film3"]
     },
     film3: {
         title: "Freedom",
         img: "assets/img/freedom.jpg",
         video: "https://www.youtube.com/embed/oHg5SJYRHA0",
+        rating: "8.5",
         rekomendasi: ["film1", "film2"]
     }
 };
